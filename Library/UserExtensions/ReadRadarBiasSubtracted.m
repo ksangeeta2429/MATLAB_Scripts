01dfa0a3bc76fd1b0179d3e072fbf20bc31d2b87
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2017-04-13 14:40:32 -0400

    Fixed comment

--- Library/UserExtensions/ReadRadarBiasSubtracted.m
+++ Library/UserExtensions/ReadRadarBiasSubtracted.m
@@ -11,6 +11,6 @@ switch outputtype
         Comp = Samp(:,1) + i * Samp(:,2);
     case 'modiplusmodq' % |I|+|Q|
         Comp = abs(Samp(:,1)) + abs(Samp(:,2));
-    otherwise % |I+Q|
+    otherwise % I+Q
         Comp = Samp(:,1) + Samp(:,2);
 end
