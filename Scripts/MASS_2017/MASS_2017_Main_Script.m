07fcec070571ab72bb42e2857ca882558223d1df
Dhrubojyoti Roy <royd@cse.osu.edu>
2017-04-18 09:50:40 -0400

    Adding 5 env trainings

--- Scripts/MASS_2017/MASS_2017_Main_Script.m
+++ Scripts/MASS_2017/MASS_2017_Main_Script.m
@@ -4,6 +4,7 @@ MASS_2017_InfoGains_and_InfoGainMAD_Training(Generate_Env_Combinations([1 2 3 4 
 MASS_2017_InfoGains_and_InfoGainMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 2),02); % Datasets 2-combo environments
 MASS_2017_InfoGains_and_InfoGainMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 3),03); % Datasets 3-combo environments
 MASS_2017_InfoGains_and_InfoGainMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 4),04); % Datasets 4-combo environments
+MASS_2017_InfoGains_and_InfoGainMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 5),05); % Datasets 5-combo environments
 
 %% mRMR_D and mRMR_MAD_D training
 
