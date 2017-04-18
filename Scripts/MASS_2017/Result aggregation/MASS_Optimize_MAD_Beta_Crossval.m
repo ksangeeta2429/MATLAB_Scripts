23c9ff4f3f4bc259ea8d6176a8c5ec8fc41af3b5
Dhrubojyoti Roy <royd@cse.osu.edu>
2017-04-17 22:51:49 -0400

    More stuff

---
+++ Scripts/MASS_2017/Result aggregation/MASS_Optimize_MAD_Beta_Crossval.m
@@ -0,0 +1,38 @@
+function Result=Mass_Optimize_MAD_Beta_Crossval(round,training_type,metric_type,prctile_param)
+cd(strcat('/home/roy.174/Dropbox/TransferPCtoMac/Round',num2str(round)));
+filters={'mRMR_D_combined', 'mRMRMAD_D_0_25s', 'mRMRMAD_D_0_5s', 'mRMRMAD_D_0_75s', 'mRMRMAD_D_1s', 'mRMRMAD_D_5s', 'mRMRMAD_D_10s', 'mRMRMAD_D_20s', 'mRMRMAD_D_50s'};
+envs=table2struct(readtable('env_processing_order.csv','ReadVariableNames',false));
+
+M={};
+for topk=10:5:40
+    crossvals=dlmread(strcat('crossval_str_',num2str(topk),'.csv'));
+    for i=1:length(envs)
+        for j=1:length(filters)
+            M=[M;{topk,envs(i).Var1,filters{j},crossvals(i,j)}];
+        end
+    end
+end
+        
+Result={};
+for i=2:9:length(M)
+    arr = [];
+    for j=i:i+7
+        arr=[arr,M{j,4}];
+    end
+    if strcmpi(metric_type,'max')==1
+        metric = min(arr(arr>=max(arr)));
+    elseif strcmpi(metric_type,'min')==1
+        metric = min(arr(arr>=min(arr)));
+    elseif strcmpi(metric_type,'median')==1
+        metric = min(arr(arr>=median(arr)));
+    elseif strcmpi(metric_type,'prctile')==1
+        metric = min(arr(arr>=prctile(arr,prctile_param)));
+    end
+    
+    for j=i:i+7
+        if M{j,4}==metric
+            Result=[Result;{M{j,1},M{j,2},M{j,3}}];
+            break
+        end
+    end
+end
