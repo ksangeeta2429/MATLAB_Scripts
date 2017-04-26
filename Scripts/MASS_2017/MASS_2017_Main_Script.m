1d55116093dddd069f9f66bce520f73ca4c86618
Dhrubojyoti Roy <royd@cse.osu.edu>
2017-04-26 13:14:55 -0400

    Commented calls to MASS_CrossEnv_Validation_script since we're not using it anymore

--- Scripts/MASS_2017/MASS_2017_Main_Script.m
+++ Scripts/MASS_2017/MASS_2017_Main_Script.m
@@ -16,10 +16,10 @@ MASS_2017_mRMR_and_mRMRMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 4)
 MASS_2017_mRMR_and_mRMRMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 5),05); % Datasets 5-combo environments
 MASS_2017_mRMR_and_mRMRMAD_Training(Generate_Env_Combinations([1 2 3 4 5 10], 6),06); % Datasets 6-combo environments
 
-%% CrossEnvironment validation re-training after above scripts are run
-MASS_CrossEnv_Validation_script(2,[10 15 20 25 30 35 40]);
-MASS_CrossEnv_Validation_script(3,[10 15 20 25 30 35 40]);
-MASS_CrossEnv_Validation_script(4,[10 15 20 25 30 35 40]);
+%% NOT DOING THESE ANYMORE - CrossEnvironment validation re-training after above scripts are run
+% MASS_CrossEnv_Validation_script(2,[10 15 20 25 30 35 40]);
+% MASS_CrossEnv_Validation_script(3,[10 15 20 25 30 35 40]);
+% MASS_CrossEnv_Validation_script(4,[10 15 20 25 30 35 40]);
 
 %% CrossEnvironment testing after above scripts are run
 
