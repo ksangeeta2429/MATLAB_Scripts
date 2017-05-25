74fcd156aea2191e0baaa4a1f211b61308850fe8
neelbhuva <[neel.7365@gmail.com]>
2017-05-25 01:23:08 -0400

    startTime and endTime implementation changed

--- Scripts/endTime.m
+++ Scripts/endTime.m
@@ -1,54 +1,18 @@
 %{
     Given start time and duration of a clip this function prints the
     endtime of the clip
-    1.42 means 1:42 PM or AM. 13.32 means 13:32 PM
-    duration is in same format, but represents time period rather than
-    absolute time. Ex: 38.36 represents 38 minutes and 36 seconds
-    Final result i.e end_time : Ex: 12.44 - 12 hrs and 44 minutes on
-    clock is the end time
+    Ex: If start time is 1:22:00, and duration is 38:36 function call should
+    be  startTime(1,22,0,38,36)
 %}
-function endTime(end_time, duration)
-    if(end_time > 12)
-        mod_value = 24;
-    else
-        mod_value = 12;
-    end
-    integer = floor(duration);
-    seconds = duration - integer;
-     %convert the seconds part to minutes
-    minutes = seconds / 60;
-    %add to integer to get the duration in minutes
-    dur_minutes = integer + minutes;
-    fprintf('Duration in minutes');
-    disp(dur_minutes);
+function endTime(start_hrs,start_min,start_secs, dur_min, dur_secs)
+    cur_time = datetime;
+    cur_year = year(cur_time);
+    cur_month = month(cur_time);
+    cur_day = day(cur_time); 
     
-    integer = floor(end_time);
-    fract = end_time - integer;
-    hrs = fract * 100 / 60;
-    start_hrs = integer + hrs;
-    fprintf('Start time in hrs');  disp(start_hrs);
-    
-    dur_hrs = (dur_minutes / 60);
-    end_hrs = start_hrs + dur_hrs;
-    fprintf('End time in hrs');  disp(end_hrs);
-    if(mod_value == 12)
-        if(end_hrs >= mod_value + 1)
-            end_hrs = end_hrs - mod_value;
-        end
-    end
-    if(mod_value == 24)
-         if(end_hrs > mod_value)
-            end_hrs = end_hrs - mod_value;
-        end
-    end
-    fprintf('End time in hrs');  disp(end_hrs);
-    
-    integer = floor(end_hrs);
-    fract = end_hrs - integer;
-    temp = fract * 60;
-    temp = temp / 100;
-    end_time = temp + integer;
-    fprintf('End time is %d hours and %d minutes',integer,temp * 100);
-    end_time = round(end_time * 100)/100;
+    start_time = datetime(cur_year, cur_month, cur_day, start_hrs, start_min, start_secs);
+    %disp(end_time);
+    end_time = start_time + minutes(dur_min);
+    end_time = end_time + seconds(dur_secs);
     disp(end_time);
 end
--- Scripts/startTime.m
+++ Scripts/startTime.m
@@ -1,57 +1,19 @@
 %{
     Given end time and duration of a video print the start time of the
     video
-    end_time should be in format Digit Digit . Digit... 
-    Ex: 1.42 means 1:42 PM or AM. 13.32 means 13:32 PM
-    duration is in same format, but represents time period rather than
-    absolute time. Ex: 38.36 represents 38 minutes and 36 seconds
-    Final result i.e start_time : Ex: 12.44 - 12 hrs and 44 minutes on
-    clock is the start time
-
-    Ex: If the end time is 1:22 PM or AM and duration is 38:36 call the function as :
-        startTime(1.22, 38.36)
+    Ex: If end time is 1:22:00, and duration is 38:36 function call should
+    be  startTime(1,22,0,38,36)
 %}
 
-function startTime(end_time, duration)
-    if(end_time > 12)
-        mod_value = 24;
-    else
-        mod_value = 12;
-    end
-    integer = floor(duration);
-    seconds = duration - integer;
-    %convert the seconds part to minutes
-    minutes = seconds / 60;
-    %add to integer to get the duration in minutes
-    dur_minutes = integer + minutes;
-    fprintf('Duration in minutes');
-    disp(dur_minutes);
+function startTime(end_hrs,end_minutes, end_secs, duration_min, duration_secs)
+    cur_time = datetime;
+    cur_year = year(cur_time);
+    cur_month = month(cur_time);
+    cur_day = day(cur_time); 
     
-    integer = floor(end_time);
-    fract = end_time - integer;
-    hrs = fract * 100 / 60;
-    end_hrs = integer + hrs;
-    fprintf('End time in hrs');  disp(end_hrs);
-    
-    dur_hrs = (dur_minutes / 60);
-    start_hrs = end_hrs - dur_hrs;
-    
-    fprintf('Start time in hrs');  disp(start_hrs);
-    if(start_hrs < 0)
-        start_hrs = mod_value + start_hrs;
-    end
-    integer = floor(start_hrs);
-    if(integer == 0)
-        start_hrs = mod_value + start_hrs;
-    end
-    fprintf('Start time in hrs');  disp(start_hrs);
-    
-    integer = floor(start_hrs);
-    fract = start_hrs - integer;
-    temp = fract * 60;
-    temp = temp / 100;
-    start_time = temp + integer;
-    fprintf('Start time is %d hours and %d minutes',integer,temp * 100);
-    start_time = round(start_time * 100)/100;
+    end_time = datetime(cur_year, cur_month, cur_day, end_hrs, end_minutes, end_secs);
+    %disp(end_time);
+    start_time = end_time - minutes(duration_min);
+    start_time = start_time - seconds(duration_secs);
     disp(start_time);
 end
