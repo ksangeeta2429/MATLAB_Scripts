d180489d1cc83613241bbd5c619ce05d270727ea
neelbhuva <[neel.7365@gmail.com]>
2017-08-15 18:16:24 -0400

    Auto label of arff file completed

--- Scripts/label_data.m
+++ Scripts/label_data.m
@@ -1,33 +1,57 @@
 %{
     This script can be used to label data in the arff file. 
-    Input file is the file which has labels in each line corresponding to each data sample in arff file. 
+    Input file is the file which has labels in each line corresponding to
+    each data sample in arff file. The input file cannot have an empty line
+    (new line). Every line should have a label.
     The labels at the end of each line in arff file will be replaced using
     the labels from the input file.
 %}
 
 
-function label_data(input_file, arff_file)
+function label_data(input_file, arff_file, arff_output)
 
-input_labels = [];
-fd = fopen(input_file, 'r');
+input_labels = []; %store the labels in an array
+all_labels = ['Human','Bike','Dog']; %use for comparision
+fd = fopen(input_file, 'r'); 
 while(~feof(fd))
-       line = fgetl(fd);
+       line = string(fgetl(fd));
+       if(line == '')
+           fprintf('Error : Found an empty line in input file. Every line should have a label.\n');
+           return;
+       end
        %disp(line);
        input_labels = [input_labels line];
 end
 fclose(fd);
+%fprintf('\n\n');
 disp(input_labels);
-fd1 = fopen(arff_file,'w+');
-for i = 1:length(input_labels)
-    tline = fgetl(fd1);
-    offset = -(length(tline)+2); 
-    tline = fgets(fd1);
-    fseek(fd1,offset,'cof'); %fd1 restored to previous line
-    if(input_labels(i) == 'Human')
-        newStr = strrep(tline,'Dog',string(input_labels(i)));
-        fprintf(fd1,'%s\n',newStr);
-    end
+
+A = regexp( fileread(arff_file), '\n', 'split');
+%disp(A(1));
+
+if(length(input_labels) >= length(A))
+    fprintf('Error : Number of labels in input file too many\n');
+    return;
 end
-fclose(fd1);
+j = 1; %index for inputlabel
+for i = 1:length(A)
+    fprintf('%d\n',contains(string(A(i)),',Dog'));
+    if(contains(string(A(i)),',Dog'))
+        if(j > length(input_labels))
+            fprintf('Error : Number of input labels less than required\n');
+            return;
+        end
+        newStr = strrep(string(A(i)),'Dog',string(input_labels(j)));
+        newStr = {newStr};
+        A(i) = newStr;
+        j = j + 1;        
+    end    
 end
 
+%write the modified file to the output file
+fd1 = fopen(arff_output,'w');
+for i = 1:length(A)
+    fprintf(fd1,'%s\n',string(A(i)));
+    %fprintf('\n');
+end
+fclose(fd1);
