33822f696e8d0fe71e4b4bca22d042c72beed2ff
neelbhuva <neel.7365@gmail.com>
2017-10-15 02:17:14 -0400

    string is absolete, label_data changed

--- Scripts/label_data.m
+++ Scripts/label_data.m
@@ -17,20 +17,21 @@ input_labels = []; %store the labels in an array
 all_labels = ['Human','Bike','Dog']; %use for comparision
 fd = fopen(input_file, 'r'); 
 while(~feof(fd))
-       line = string(fgetl(fd));
-       if(line == '')
+       line = cellstr(fgetl(fd));
+       %disp(line);
+       if(isempty(line))
            fprintf('Error : Found an empty line in input file. Every line should have a label.\n');
            return;
        end
        %disp(line);
-       input_labels = [input_labels line];
+       input_labels = [input_labels, line];
 end
 fclose(fd);
 %fprintf('\n\n');
-disp(input_labels);
+%disp(input_labels);
 
 A = regexp( fileread(arff_file), '\n', 'split');
-%disp(A(1));
+%disp(A);
 
 if(length(input_labels) >= length(A))
     fprintf('Error : Number of labels in input file too many\n');
@@ -38,23 +39,26 @@ if(length(input_labels) >= length(A))
 end
 j = 1; %index for inputlabel
 for i = 1:length(A)
-    fprintf('%d\n',contains(string(A(i)),',Dog'));
-    if(contains(string(A(i)),',Dog'))
+    a = A{i};
+    index = strfind(a,',Dog');
+    fprintf('%d',length(index));
+    if(length(index) == 1)
         if(j > length(input_labels))
             fprintf('Error : Number of input labels less than required\n');
             return;
         end
-        newStr = strrep(string(A(i)),'Dog',string(input_labels(j)));
+        newStr = strrep(A(i),'Dog',char(input_labels(j)));
+        disp(newStr);
         newStr = {newStr};
         A(i) = newStr;
         j = j + 1;        
     end    
 end
-
+fprintf('\n');
 %write the modified file to the output file
 fd1 = fopen(arff_output,'w');
 for i = 1:length(A)
-    fprintf(fd1,'%s\n',string(A(i)));
+    fprintf(fd1,'%s\n',char(A{i}));
     %fprintf('\n');
 end
 fclose(fd1);
