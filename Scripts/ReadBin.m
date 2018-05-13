f828b1475965da02ec157fd3202d9ad1c29c6a02
Michael McGrath <mcgrath.57@osu.edu>
2018-05-13 07:28:20 -0400

    ReadBin: add input check. throw proper warning.

--- Scripts/ReadBin.m
+++ Scripts/ReadBin.m
@@ -1,10 +1,17 @@
-function Data = ReadBin(File)
-
+function [ Data ] = ReadBin( File )
 % ReadBin -- Reads raw data from a data sample file (int16's).
+
+if nargin < 1 || exist('File','var') ~= 1 || isempty(File)
+    error('Expected input filename.');
+end
+
+if ~ischar(File)
+    error( 'Expected char vector input of file name.' );
+end
 
 Fid = fopen(File, 'r');
 if (Fid < 0)
-  'Could not open file'
+    warning( [ 'Could not open file ' File ] );
 end
 
 Data = fread(Fid, inf, 'int16');
