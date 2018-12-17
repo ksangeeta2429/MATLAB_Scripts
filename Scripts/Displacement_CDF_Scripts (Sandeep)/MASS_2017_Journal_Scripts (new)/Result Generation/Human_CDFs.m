5a0ebf44ef869766103df4d95376dc46ca9360fd
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2018-12-17 01:36:50 -0500

    Adding a combined target CDF graphing script Humans, balls, cars

--- Scripts/Displacement_CDF_Scripts (Sandeep)/MASS_2017_Journal_Scripts (new)/Result Generation/Human_CDFs.m
+++ Scripts/Displacement_CDF_Scripts (Sandeep)/MASS_2017_Journal_Scripts (new)/Result Generation/Human_CDFs.m
@@ -17,26 +17,3 @@ xlim([0 0.6]);
 
 saveas(h,strcat('/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Results/Displacement_Graphs/Graphs/Human_CDFs_N=',num2str(N),'_IQR=',num2str(IQRejectionParam),'.fig'));
 saveas(h,strcat('/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Results/Displacement_Graphs/Graphs/Human_CDFs_N=',num2str(N),'_IQR=',num2str(IQRejectionParam),'.eps'),'eps2c');
-
-
-% combined CDF
-figure;
-
-h_comb=WalkCdfs_Individual_MASS(["/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Data_Repository/IPSNdata/arc_1/Human",...
-                             "/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Data_Repository/IPSNdata/prb_2/Human",...
-                             "/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Data_Repository/Parking garage radial ortho (Sandeep)/SenSys10_data_scripts/data/0-amplitude (walks)/ortho/cut",...
-                             "/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Data_Repository/Parking garage radial ortho (Sandeep)/SenSys10_data_scripts/data/0-amplitude (walks)/radial/runs/cut"],...
-                             '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Results/Displacement_Graphs/Walk_CDFs/','radial_7m',250,0.9,N);
-%h4=WalkCdfs_Individual_MASS('/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Data_Repository/Darree_Fields/cut','/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Results/Displacement_Graphs/Walk_CDFs/','Darree_fields',250,0.9,N);
-
-h=gca;
-h.XLabel.String = 'Distance (meters)';
-h.YLabel.String = 'CCDF';
-xlim([0 1]);
-%legend([h0 h1 h2 h3],{'Gymnasium','Building foyer','Garage (orthogonal)','Garage (radial)','Forest'});
-title(sprintf('Human CDFs for\n%d 1/2-second Windows(IQR Parameter=%0.2f)',N,IQRejectionParam),'FontWeight','normal');
-xlim([0 0.6]);
-
-saveas(h,strcat('/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Results/Displacement_Graphs/Graphs/Comb_human_CDFs_N=',num2str(N),'_IQR=',num2str(IQRejectionParam),'.fig'));
-saveas(h,strcat('/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Robust_Learning/Results/Displacement_Graphs/Graphs/Comb_human_CDFs_N=',num2str(N),'_IQR=',num2str(IQRejectionParam),'.eps'),'eps2c');
-
