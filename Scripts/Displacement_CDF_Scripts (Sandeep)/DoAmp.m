78172b3c97a4735990aa21f05f6d1ae6b71857db
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2018-12-16 23:19:17 -0500

    Removing Displacement CDF scripts (see master for latest)

--- Scripts/Displacement_CDF_Scripts (Sandeep)/DoAmp.m
+++
@@ -1,7 +0,0 @@
-function DoAmp(File, StopDist)
-
-Path = 'Data/0-Amplitude';
-Comp = ReadComp(sprintf('%s/%s', Path,File));
-
-disp(sprintf('Selted the walkin that stops at %d m',StopDist));
-FitAmp(Comp, StopDist, 25,50);
--- Scripts/Displacement_CDF_Scripts (Sandeep)/MASS_2017_Journal_Scripts (new)/ComputeNoiseCDFs_MASS.m
+++
@@ -1,4 +0,0 @@
-function OutDataFile=ComputeNoiseCDFs_MASS(inFile, outPath, outName, SampRate, deltaT, IQRejectionParam,N)
-Comp = ReadRadar(inFile);
-UnRots=GetRawUnrots(Comp,SampRate,IQRejectionParam);
-[~,OutDataFile]=NoiseCdf_MASS(UnRots, outPath, outName, SampRate, deltaT, IQRejectionParam,N);
